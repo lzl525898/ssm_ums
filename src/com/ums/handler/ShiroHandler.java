6d3039e59fdbedf0daac8d709c54b64f1b9bde06
luckyforlei <luckyforlei@163.com>
2018-03-15 17:39:13 +0800

    update

--- src/com/ums/handler/ShiroHandler.java
+++ src/com/ums/handler/ShiroHandler.java
@@ -6,6 +6,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.apache.tomcat.util.buf.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,16 +22,20 @@ public class ShiroHandler {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
-	public ReqResult login(@RequestParam("username") String username, @RequestParam("password") String password) {
+	public ReqResult login(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("rememberMe")String rememberMe) {
+		if ("on".equals(rememberMe)) {
+			System.out.println("ok");
+		} else {
+			System.out.println("no");
+		}
 		Subject currentUser = SecurityUtils.getSubject();
 		if (!currentUser.isAuthenticated()) {
 			// 把用户名封装成UsernamePasswordToken对象
 			UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-			token.setRememberMe(true);
+	//		token.setRememberMe(true);
 			try {
 				currentUser.login(token);
 			} catch (AuthenticationException e) {
-				// TODO Auto-generated catch block
 				return ReqResult.fail().add("info", GeneratingHTML.genLoginError());
 			}
 		}
